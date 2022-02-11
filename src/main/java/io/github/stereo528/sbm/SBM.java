07099d0619e1f5164771271d86c0f37279b9efa2
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-02-10 22:09:40 -0700

    logging, logo, correct license

--- src/main/java/io/github/stereo528/sbm/SBM.java
+++ src/main/java/io/github/stereo528/sbm/SBM.java
@@ -1,10 +1,22 @@
 package io.github.stereo528.sbm;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.Level;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
 
 public class SBM implements ModInitializer {
+
+    public static final String MOD_ID = "sbm";
+    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().getFriendlyString();
+    public static final String MOD_NAME = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getName();
+
+    static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
     @Override
     public void onInitialize() {
-
+        SBM.LOGGER.info("poggy woggy");
     }
 }
